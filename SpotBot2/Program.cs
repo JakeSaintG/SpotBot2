df6cfb138e7bd5ce66f43c3b888158b71a110dc7
Jake Saint Germain <jakesaint27@gmail.com>
2022-10-06 13:48:15 -0400

    Initial setup

---
+++ SpotBot2/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+	private DiscordSocketClient _client;
+
+	public static Task Main(string[] args) => new Program().MainAsync();
+
+	public async Task MainAsync()
+	{
+        _client = new DiscordSocketClient();
+
+        var token = File.ReadAllText("./token.txt");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+}
